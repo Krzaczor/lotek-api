49fcb85459a7fc9bd7ed75f0446d68458c6a469e
Krzaczor <jbielecki18@gmail.com>
2020-04-18 21:25:39 +0200

    create error messages in validate body

--- src/app.js
+++ src/app.js
@@ -2,19 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import drawsController from './draws/controller';
-import numbersController from './numbers/controller';
+// import numbersController from './numbers/controller';
 
 const app = express();
 const optionsCors = {
     origin: 'http://localhost:3001',
-    methods: 'GET',
-    exposedHeaders: 'Content-Length'
+    methods: 'GET'
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/draws', cors(optionsCors), drawsController);
-app.use('/numbers', cors(optionsCors), numbersController);
+// app.use('/numbers', cors(optionsCors), numbersController);
 
 module.exports = app;
--- src/middlewares/validators/body.js
+++ src/middlewares/validators/body.js
@@ -2,43 +2,50 @@ import Joi from '@hapi/joi';
 import * as setting from '../../setting';
 
 //number schema in numbers array
-const JoiNumberSchema = Joi
-    .number()
+const JoiNumberSchema = Joi.number()
     .integer()
     .min(setting.minNumber)
     .max(setting.maxNumber)
-    .required()
-// .error(() => {
-//     throw new Error(`Joi liczby muszą być całkowite oraz z przedziału od ${setting.minNumber} do ${setting.maxNumber}`)
-// })
+    .messages({
+        'number.base': 'Podaj same liczby',
+        'number.min': `Największą liczbą musi być ${setting.minNumber}`,
+        'number.max': `Najmnieszją liczbą musi być ${setting.maxNumber}`,
+    });
 
 const checkRepeatValues = (a, b) => a === b;
-// if (a === b) {
-//     throw new Error('Joi: liczby nie mogą się powtarzać.')
-// }
-// };
 
 //numbers schema in body request
-const JoiNumbersSchema = Joi
-    .array()
+const JoiNumbersSchema = Joi.array()
     .length(setting.lengthDraw)
+    .empty()
     .items(JoiNumberSchema)
     .unique(checkRepeatValues)
     .required()
+    .messages({
+        'array.base': 'Podano niepoprawne dane',
+        'any.required': 'Nie podano liczb',
+        'any.empty': 'Nie podano żadnych liczb',
+        'array.length': `Podaj dokładnie ${setting.lengthDraw} liczb`,
+        'array.unique': 'Liczby nie mogą się powtarzac'
+    })
 
 //time schema in body request
-const JoiTimeSchema = Joi
-    .date()
+const JoiTimeSchema = Joi.date()
+    .iso()
     .min(setting.minDateAsInt)
     .required()
-// .error(() => {
-//     throw new Error('Joi: nieprawidłowa data losowania')
-// })
+    .messages({
+        'any.required': 'Nie podano daty',
+        'date.base': 'Niepoprawna data',
+        'date.format': 'Niepoprawny format daty',
+        'date.min': 'Data nie może być mniejsza niż 26.01.1957'
+    })
 
 //body schema in request POST /draws
-const JoiBodySchema = Joi.object().keys({
-    time: JoiTimeSchema,
-    numbers: JoiNumbersSchema
-}).options({ abortEarly: false });
+const JoiBodySchema = Joi
+    .object().keys({
+        time: JoiTimeSchema,
+        numbers: JoiNumbersSchema
+    })
 
-export default (body) => JoiBodySchema.validateAsync(body);
+export default (body) => JoiBodySchema.validateAsync(body, { abortEarly: false });
