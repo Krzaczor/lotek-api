680df2b9f284683ec8fa2f10c24f1e4edd769455
Krzaczor <jbielecki18@gmail.com>
2020-04-21 15:49:35 +0200

    install detect-port. Choose unused port

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import detect from 'detect-port';
 import app from './src/app';
 
-const connect = (ip, port, dbname) => {
-    mongoose.connect(`mongodb://${ip}:${port}/${dbname}`, {
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+const connect = async (ip, port, dbname) => {
+    await mongoose.connect(`mongodb://${ip}:${port}/${dbname}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     });
 
     const listener = app.listen(app.get('port'), () => {
-        console.log(`server http://localhost:${listener.address().port} started`);
+        console.log(`server http://${HOST}:${listener.address().port} started`);
     });
+}
+
+const setPort = async () => {
+    app.set('port', process.env.PORT || await detect(DEFAULT_PORT));
 }
 
 mongoose.Promise = global.Promise;
 
 dotenv.config();
-app.set('port', process.env.PORT || 3000);
+setPort();
 connect(process.env.DB_IP, process.env.DB_POST, process.env.DB_NAME);
